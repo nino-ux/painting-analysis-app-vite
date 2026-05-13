bfaa93701a4e2d13f7b5efcf88e27ff86b758562
nino-ux <nikita.novik.ux@gmail.com>
2026-05-13 10:17:40 -0600

    Figuring out the element runtime

--- src/components/CategoryTree.jsx
+++ src/components/CategoryTree.jsx
@@ -5,6 +5,9 @@ import { useState, useEffect } from 'react'
 //import Data
 import currentPaintingData from "../data/StarryNight.json";
 import paintingDataDefault from "../data/CategoryTree.json"
+import ElementDot from '../data/ElementDot.json'
+import DefaultRuntime from '../data/ElementRuntime.json'
+
 
 //import Components
 import QuestionModal from './QuestionModal';
@@ -53,16 +56,39 @@ function CategoryTree() {
 
     const { currentElement, handleElementClick } = useClickTracker();
 
-        const [elementRuntime, setElementRuntime] = useState(initialRuntime);
+    // const elIndex = props.currentPaintingData.elements.find(el => el.name === subelement.name);
+
+    // let type;
+    // if (elIndex.clue) {
+    //     type = "clue"
+    // } else { type = "regular" }
+
+    // const elDot = ElementDot.find(dot => dot.name === type);
+
+    const initialRuntime = currentPaintingData.elements.map(el => {
+        let type;
+        if (el.clue) {
+            type = "clue"
+        } else { type = "regular" }
+
+        const elDot = ElementDot.find(dot => dot.name === type);
+
+        return {
+            ...DefaultRuntime,
+            name: el.name,
+            type: type,
+            dot: elDot.state
+        }
+    })
+
+    const [elementRuntime, setElementRuntime] = useState(initialRuntime);
+
 
 
     useEffect(() => {
-        console.log(currentElement)
-    }, [currentElement])
+        console.log(initialRuntime)
+    }, [initialRuntime])
 
-    useEffect(() => {
-        console.log(elementRuntime)
-    }, [currentElement])
 
 
     return (
@@ -83,6 +109,9 @@ function CategoryTree() {
                                                     currentElement={currentElement}
                                                     categoryTreeData={categoryTreeData}
                                                     currentPaintingData={currentPaintingData}
+                                                    useClickTracker={useClickTracker}
+                                                    elementRuntime={elementRuntime}
+                                                    setElementRuntime={setElementRuntime}
                                                 />
                                                 <a
                                                     onClick={handleElementClick}
--- src/components/DotIndicator.jsx
+++ src/components/DotIndicator.jsx
@@ -1,37 +1,13 @@
 import { useEffect, useState } from 'react'
-import ElementDot from '../data/ElementDot.json'
-import DefaultRuntime from '../data/ElementRuntime.json'
+
 
 
 function DotIndicator(props) {
 
-    const elIndex = props.currentPaintingData.elements.find(el => el.name === props.subelement.name);
-
-    let type;
-    if (elIndex.clue) {
-        type = "clue"
-    } else { type = "regular" }
-
-    const elDot = ElementDot.find(dot => dot.name === type);
-
-    const initialRuntime =
-    {
-        ...DefaultRuntime,
-        name: elIndex.name,
-        type: type,
-        dot: elDot.state
-    }
-
-    const [elementRuntime, setElementRuntime] = useState(initialRuntime);
-
-    const isActive = (elementRuntime.name === props.currentElement)
-
-        useEffect(() => {
-            console.log(elementRuntime)
-        }, [currentElement])
+    const isActive = (props.elementRuntime.name === props.currentElement)
 
     return (
-        <span>{ isActive ? elementRuntime.dot[1] : elementRuntime.dot[0] }</span>
+        <span>{ isActive ? props.elementRuntime.dot[1] : props.elementRuntime.dot[0] }</span>
     )
 }
 
